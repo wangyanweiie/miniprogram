767cf1b98fed3d0daf0b3462361195e79dde385f
wangyanweiie <740484522@qq.com>
2021-10-01 08:48:54 +0800

    修改配置信息

---
+++ pages/components/eves/eves.js
@@ -0,0 +1,10 @@
+
+Component({
+  methods: {
+    addCount(){
+      //通过 triggerEvent 发出自定义事件,让使用该组件的页面监听到;
+      //* this.triggerEvent('事件名',{参数对象},{options})
+      this.triggerEvent('addCount')
+    }
+  }
+})
