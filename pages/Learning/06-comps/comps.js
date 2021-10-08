496e975c779edb0077ed1f66a64c9357bfef9df0
wangyanweiie <740484522@qq.com>
2021-10-08 19:44:42 +0800

    Learning

--- pages/Learning/06-comps/comps.js
+++ pages/Learning/06-comps/comps.js
@@ -9,5 +9,17 @@ Page({
       count: this.data.count + 1
     });
     console.log(event);
+  },
+  /**
+  直接操作组件内的属性或者方法
+  * 首先获取到组件对象: const cpn = this.selectComponent("#id/.class")
+  * 其次调用组件对象的setData方法: cpn.setData({..})
+  **/
+  increase(){
+    const mySel = this.selectComponent('#my-sel')
+    mySel.setData({
+      sum: mySel.data.sum + 1
+    })
+
   }
 })
---
+++ pages/components/my-sel/my-sel.js
@@ -0,0 +1,11 @@
+Component({
+  //组件的属性列表
+  properties: {
+
+  },
+
+  //组件的初始数据
+  data: {
+    sum:0
+  },
+})
