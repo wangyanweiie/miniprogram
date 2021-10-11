0d930c2d57ad549f9b5db04272b996e9cb583123
wangyanweiie <740484522@qq.com>
2021-10-11 09:33:24 +0800

    Learning

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,53 +1,25 @@
 Page({
   //*****************页面的初始数据*****************
   data: {
-
+    homeData:{}
   },
-  //***************自定义事件响应函数***************
+  //***自定义事件响应函数***
   //tab-control组件发出自定义事件的响应函数
   titleClick(event){
     console.log(`索引为${event.detail.index}的--${event.detail.title}--发生了点击`);
   },
 
-  //******************生命周期函数******************
+  //******生命周期函数******
   //监听页面加载
-  onLoad: function (options) {
-
-  },
-
-  //监听页面初次渲染完成
-  onReady: function () {
-
-  },
-
-  //监听页面显示
-  onShow: function () {
-
-  },
-
-  //监听页面隐藏
-  onHide: function () {
-
-  },
-
-  //监听页面卸载
-  onUnload: function () {
-
-  },
-
-  //**************页面相关事件处理函数**************
-  //监听用户下拉动作
-  onPullDownRefresh: function () {
-
-  },
-
-  //页面上拉触底事件的处理函数
-  onReachBottom: function () {
-
-  },
-
-  //用户点击右上角分享
-  onShareAppMessage: function () {
-
+  onLoad(options) {
+    //1.发送网络请求获取服务器数据(箭头函数获取到正确的this指向):url地址需要先进行域名配置
+    wx.request({
+      url: 'http://152.136.185.210:7878/api/hy66/recommend',
+      success: (res)=>{
+        this.setData({
+          homeData: res.data.data
+        })
+      }
+    })
   }
 })
